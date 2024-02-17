58920da5f29daf3822f5aa1b5b27d51004cd6d80
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2024-02-17 16:53:41 +0100

    adicionando al

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,39 +8,9 @@ import { fazendasRoutes } from "./FazendaRoute";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from '../../swagger.json'
-import swaggerJSDoc from "swagger-jsdoc";
 import express from "express"
 import path from "path"
 
-
-const options = {
-    definition: {
-      openapi: "3.1.0",
-      info: {
-        title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
-        description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        },
-        contact: {
-          name: "LogRocket",
-          url: "https://logrocket.com",
-          email: "info@email.com",
-        },
-      },
-      servers: [
-        {
-          url: "http://localhost:3333",
-        },
-      ],
-    },
-    apis: ["./routes/*.js"],
-};
-  
-const spec = swaggerJSDoc(options);
 
 const routes = Router();
 const __dirname = path.resolve();
